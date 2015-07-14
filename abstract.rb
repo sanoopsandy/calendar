95b0f6c298201df9f4b820da2ccadc73bae355b9
sanoopsandy <sanoop@vacationlabs.com>
2015-07-14 10:24:20 +0530

    Abstract calendar

--- abstract.rb
+++ abstract.rb
@@ -1,23 +1,19 @@
 require 'date'
 require 'io/console'
 require 'optparse'
-
+require 'csv'
 date = Date.today
 month_name =Date::ABBR_MONTHNAMES
 
-$holiday = [[Date.new(2015,1,1)," New Year "," "],
-            [Date.new(2015,1,26) ,"Republic Day"," "],
-            [Date.new(2015,2,14),"Valentines Day"," "],
-            [Date.new(2015,7,19)," Ramzan"," "],
-            [Date.new(2015,7,31)," Jamat Ul Vida"," "],
-            [Date.new(2015,8,15)," Independance Day "," "],
-            [Date.new(2015,8,28)," Onam "," "],
-            [Date.new(2015,9,17)," Ganesh Chaturthi"," "],
-            [Date.new(2015,10,22)," Dussehra"," "],
-            [Date.new(2015,11,11)," Diwali"," "],
-            [Date.new(2015,12,25)," Christmas "," "],
-            [Date.new(2016,1,1), "New Year "," "],
-            [Date.new(2016,1,26)," Republic Day"," "]]
+$holiday = [ ]
+	if File.exist?('holiday.csv') 
+		CSV.foreach('holiday.csv') do |row|    
+    	$holiday.push(row)
+    end
+	else
+		puts " No CSV file found "
+	end
+
 
 def month_len (year,month)
   	time = Date.new(year,month,-1)
@@ -49,8 +45,8 @@ def hol (month,year)
     ch = 'a'
     holiday2 = $holiday
     for i in 0..12
-        if $holiday [i][0].mon == month && $holiday[i][0].year == year
-            holiday2[i][2] = ch
+        if $holiday [i][1].to_i== month && $holiday[i][0].to_i== year
+            holiday2[i][4] = ch
             temp.push(holiday2[i])
             ch=ch.next
         end
@@ -61,8 +57,8 @@ end
 def hol_list (month,year)
     temp_hol = []
     for i in 0..12
-        if $holiday [i][0].mon == month && $holiday[i][0].year == year
-            temp_hol.push($holiday[i][1])
+        if $holiday [i][1].to_i == month && $holiday[i][0].to_i == year
+                temp_hol.push($holiday[i][3])
         end
     end
     return temp_hol
@@ -91,14 +87,14 @@ end
 
 holiday = lambda do |day_count,month,year|	
     temp_hol = hol(month,year)
-    len = temp_hol.length
-    temp_hol1 = []
+	len = temp_hol.length
+	temp_hol1 = []
     for i in 0..len-1
-        if temp_hol[i][0].day==day_count
-           temp_hol1.push(temp_hol[i][2])
+        if temp_hol[i][2].to_i==day_count
+            temp_hol1.push(temp_hol[i][4])
         end
     end
-    return temp_hol1[0].to_s
+    return temp_hol1[0]
 end
 
 flags = lambda do |h,day_count,cur_month,month,year,j| 
@@ -129,13 +125,13 @@ def create_calendar(month,year,start_dow,flags,h)
 		for j in 0..6
 			if i == 0 
 				if j>=count && day_count<= len
-                    printf("%5s",day_count.to_s+flags.call(h,day_count,month,month,year,j))
+                    printf("%5s",day_count.to_s+flags.call(h,day_count,month,month,year,j).to_s)
 					day_count +=1
 				else
 					printf("%5s",prev_mon_date(weekday,start_dow+j,prev_len).to_s+flags.call(h,day_count,month,month-1,year,j))
 				end
 			elsif day_count<= len
-					printf("%5s",day_count.to_s+flags.call(h,day_count,month,month,year,j))
+					printf("%5s",day_count.to_s+flags.call(h,day_count,month,month,year,j).to_s)
 					day_count +=1				
 			else
 				next_mon += 1
@@ -149,7 +145,6 @@ end
 options = {}
 OptionParser.new do |opts|
   opts.banner = "Usage: example.rb [options]"
-
   opts.on("-m", "--m Month", "Month") do |m|
     options[:month] = m
   end
@@ -162,19 +157,16 @@ OptionParser.new do |opts|
   opts.on("-h", "--hholiday", "holiday") do |h|
     options[:holiday] = h
   end
-end.parse! 
-
-  
-
+end.parse!
 
 puts "\e[H\e[2J"
 month = options [:month].to_i
 year =  options [:year].to_i
 w =  options [:w].to_i
 h = options[:holiday]
-if !options[:month] or !options[:year]
+	if !options[:month] or !options[:year]
 		puts " Month and Year not mentioned "
-  else
+  	else
 	print "\t \t",year
 	puts "\n"
 	print "\t \t",month_name[month]
